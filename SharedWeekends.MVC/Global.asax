4f3a29ee5988a8dafe2c156e52c02a90c983d82e
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-11-04 15:12:47 +0200

    initial scaffolded project

---
+++ SharedWeekends.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SharedWeekends.MVC.MvcApplication" Language="C#" %>
---
+++ SharedWeekends.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharedWeekends.MVC.Startup))]
+namespace SharedWeekends.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
