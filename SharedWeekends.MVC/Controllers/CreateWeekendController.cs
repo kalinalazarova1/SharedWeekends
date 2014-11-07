b491844f1604d092b76e8152d46acf7665ab7199
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-11-07 11:28:00 +0200

    sample form for create weekend

---
+++ SharedWeekends.MVC/Controllers/CreateWeekendController.cs
@@ -0,0 +1,29 @@
+﻿using SharedWeekends.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SharedWeekends.MVC.Controllers
+{
+    public class CreateWeekendController : BaseController
+    {
+        public CreateWeekendController(IWeekendsData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
