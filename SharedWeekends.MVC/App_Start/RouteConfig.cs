0ea2d43409ae689b0f04c5299c9849b2a9ac6236
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-11-25 02:31:47 +0200

    last last

--- SharedWeekends.MVC/App_Start/RouteConfig.cs
+++ SharedWeekends.MVC/App_Start/RouteConfig.cs
@@ -8,6 +8,7 @@
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
             
             routes.MapRoute(
                 name: "Default",
