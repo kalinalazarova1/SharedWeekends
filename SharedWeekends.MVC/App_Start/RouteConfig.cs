822c99d33a56e4e4ef72171ac2074700120ba393
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-11-25 08:51:39 +0200

    changed MVC project file removed automapper import

--- SharedWeekends.MVC/App_Start/RouteConfig.cs
+++ SharedWeekends.MVC/App_Start/RouteConfig.cs
@@ -11,25 +11,10 @@
             routes.IgnoreRoute("");
             
             routes.MapRoute(
-                name: "Default",
-                url: "",
-                defaults: new { controller = "Home", action = "Index" },
-                namespaces: new[] { "SharedWeekends.MVC.Controllers" });
-
-            routes.MapRoute(
                 name: "Default1",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 namespaces: new[] { "SharedWeekends.MVC.Controllers" });
-
-            //routes.MapRoute(
-            //    name: "Static",
-            //    url: "{action}",
-            //    defaults: new { controller = "Home" },
-            //    namespaces: new[] { "SharedWeekends.MVC.Controllers" });
-            //
-            
-
         }
     }
 }
