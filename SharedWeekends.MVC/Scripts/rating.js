cdc90a267f14f2d9251c52300dc08e0621675154
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-11-25 15:18:17 +0200

    small fix

--- SharedWeekends.MVC/Scripts/rating.js
+++ SharedWeekends.MVC/Scripts/rating.js
@@ -1,6 +1,6 @@
 ﻿$('#postReview').on('click', function () {
     var reviewsText = $('#reviewsCount').html();
     var reviewsIndex = reviewsText.indexOf(' reviews');
-    var reviewsCount = parseInt(text.substr(0, reviewsIndex)) + 1;
+    var reviewsCount = parseInt(reviewsText.substr(0, reviewsIndex)) + 1;
     $('#reviewsCount').html(reviewsCount + ' reviews');
 });
